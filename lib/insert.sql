0680c9d9f8eeab906586604becbcdccfd0b4ee46
billyott <willrichott@gmail.com>
2020-09-22 15:02:01 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,140 @@
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Mr. Chocolate",
+    5,
+    "F",
+    "White",
+    "Really Nice",
+    1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Rowdy",
+    3,
+    "M",
+    "Non-White",
+    "kinda mean",
+    1
+);
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Tabitha",
+    2,
+    "M",
+    "Rainbow",
+    "Awkward",
+    0
+);
+
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Sergeant Brown",
+    1,
+    "M",
+    "Skin color (No hair)",
+    "just ok",
+    1
+);
+
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Melissa",
+    57,
+    "M",
+    "Off White",
+    "can't hear very well",
+    1
+);
+
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Grinch",
+    10,
+    "M",
+    "Green",
+    "Grinchy",
+    1
+);
+
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    "Wendy",
+    2,
+    "F",
+    "Brown",
+    "Sweet Girl, but dead",
+    0
+);
+
+
+INSERT INTO bears (
+  name,
+  age,
+  gender,
+  color,
+  temperament,
+  alive
+)
+VALUES (
+    NULL,
+    100,
+    "M",
+    "We don't know",
+    "Murdery",
+    1
+);
